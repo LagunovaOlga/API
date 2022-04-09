14f5077fc4a465d2427d6bbaafbfa27eb7bf4d09
LagunovaOlga <Skynet-irbit@yandex.ru>
2022-04-09 11:45:48 +0500

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
